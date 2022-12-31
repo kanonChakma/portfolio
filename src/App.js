3dd0d83012f703e9bc27fdb896ca5bbf5d3c4268
kanon chakma <kanonchakma@pop-os.localdomain>
2022-12-31 18:30:26 +0600

    feat: add services section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import About from './components/About';
 import Header from './components/Header';
 import Home from './components/Home';
+import Services from './components/Services';
 import Skills from './components/Skills';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Home/>
         <About/>
         <Skills/>
+        <Services/>
       </main>
     </div>
   );
