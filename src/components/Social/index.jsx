a310dc03a7fec726bb5992c524cb6728224c1457
kanon chakma <kanonchakma@pop-os.localdomain>
2023-01-04 22:42:28 +0600

    add footer

--- src/components/Social/index.jsx
+++ src/components/Social/index.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
 
 const Social = () => {
+  
   return (
      <div className='home__social'>
-         <a href='/' className='home__social-icon'  target="_blank">
+         <a href="https://www.instagram.com/"
+         className='home__social-icon'  
+         target="blank">
             <i className='uil uil-instagram'></i>
          </a>
          
-         <a href='/' className='home__social-icon' target="_blank">
+         <a href='https://dribble.com' 
+         className='home__social-icon' 
+         target="blank">
            <i className='uil uil-dribbble'></i>
          </a>
 
-         <a href='/' className='home__social-icon' target="_blank">
+         <a href='https://github.com'
+           className='home__social-icon' 
+           target="blank">
            <i className='uil uil-github-alt'></i>
          </a>
      </div>
